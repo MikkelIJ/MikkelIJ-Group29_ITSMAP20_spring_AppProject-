d8a03240df8de732ccd0109bd58a52ba3e8057b2
KristianSP93 <Ksp-93@hotmail.com>
2020-04-27 13:22:49 +0200

    Creating API call in UserActivity

--- app/src/main/java/com/smap/group29/getmoving/Activities/UserActivity.java
+++ app/src/main/java/com/smap/group29/getmoving/Activities/UserActivity.java
@@ -3,12 +3,27 @@ package com.smap.group29.getmoving.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.smap.group29.getmoving.R;
 import com.smap.group29.getmoving.Utils.FirebaseUtil;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
 
 public class UserActivity extends AppCompatActivity {
+
+    private TextView tv_weatherTemp, tv_weatherDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +31,50 @@ public class UserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
        FirebaseUtil.openFirebaseReference("TheNewRealDeal",this);
+
+        tv_weatherTemp = findViewById(R.id.tv_WeatherTemp);
+        tv_weatherDescription = findViewById(R.id.tv_Weather);
+
+        findWeather();
+
+    }
+
+    private void findWeather() {
+        String url = "http://api.openweathermap.org/data/2.5/weather?id=2624652&appid=a1b9c2c209a84cd50be4c25fbcd02a88&units=metric";
+
+        JsonObjectRequest jor = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try {
+                    JSONObject main_object = response.getJSONObject("main");
+                    JSONArray array = response.getJSONArray("weather");
+                    JSONObject object = array.getJSONObject(0);
+                    String temp = String.valueOf(main_object.getDouble("temp"));
+                    String description = object.getString("description");
+                    String city = object.getString("name");
+
+                    tv_weatherTemp.setText(temp);
+                    tv_weatherDescription.setText(description);
+
+
+                }
+                catch (JSONException e){
+                    e.printStackTrace();
+
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }
+        );
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jor);
+
     }
 
 
@@ -30,4 +89,6 @@ public class UserActivity extends AppCompatActivity {
         super.onResume();
         FirebaseUtil.attachListener();
     }
+
+
 }
