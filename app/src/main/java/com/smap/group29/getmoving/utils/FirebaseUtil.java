86a7111b8a2d05e9851f0b84d3f3b9511b652470
IHCoding <s.imal@yahoo.com>
2020-05-03 12:21:54 +0200

    weather icon fixed.

---
+++ app/src/main/java/com/smap/group29/getmoving/utils/FirebaseUtil.java
@@ -0,0 +1,69 @@
+package com.smap.group29.getmoving.utils;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FirebaseUtil {
+
+    private static FirebaseUtil firebaseUtil;
+    private static FirebaseDatabase mFirebaseDatabase;
+    private static DatabaseReference mDatabaseReference;
+    public static FirebaseAuth mFirebaseAuth;
+    public static FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    private static Activity caller;
+
+    public FirebaseUtil(){}
+
+    public static void openFirebaseReference(String ref, final Activity callerActivity){
+        if (firebaseUtil == null){
+            firebaseUtil = new FirebaseUtil();
+            mFirebaseDatabase = FirebaseDatabase.getInstance();
+            mFirebaseAuth = FirebaseAuth.getInstance();
+            caller = callerActivity;
+
+            mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                    // check if the user is logged in else don't need to
+                    if (firebaseAuth.getInstance().getCurrentUser() == null ){ FirebaseUtil.signIn(); }
+                    Toast.makeText(callerActivity.getBaseContext(), "Welcome back!", Toast.LENGTH_LONG).show();
+                }
+            };
+        }
+        mDatabaseReference = mFirebaseDatabase.getReference().child(ref);
+    }
+
+    private static void signIn(){
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        // Create and launch sign-in intent
+        caller.startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                1);
+    }
+
+    public static void attachListener() {
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    public static void detachListener(){
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+}
